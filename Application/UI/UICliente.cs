11cda01e5d5c0ed1bda800f139c4d37c62139628
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-05-02 16:53:27 -0500

    feat: :construction: UI Cliente

---
+++ Application/UI/UICliente.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using MiAppHexagonal.Application.Services;
+using MiAppHexagonal.Domain.Factory;
+
+namespace MiAppHexagonal.Application.UI.Clientes
+{
+    public class UICliente
+    {
+        
+      
+        string connStr = "server=localhost;database=prueba;user=root;password=campus2023;";
+       
+        private readonly IDbFactory _factory;
+        //var servicio = new ClienteService(factory.CrearClienteRepository());
+
+        public UICliente(IDbFactory factory)
+        {
+            _factory = factory;
+
+
+        }
+    }
+
+}
